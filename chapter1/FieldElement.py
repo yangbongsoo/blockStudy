6658547622217068e2ae546dfde7b2083ba25019
yang bongsoo <goodbs1000@gmail.com>
2022-09-10 23:20:56 +0900

    first commit

---
+++ chapter1/FieldElement.py
@@ -0,0 +1,59 @@
+class FieldElement:
+
+	def __init__(self, num, prime):
+		if num >= prime or num < 0:
+			error = 'Num {} not in field range 0 to {}'.format(num, prime - 1)
+			raise ValueError(error)
+		self.num = num
+		self.prime = prime
+
+	def __repr__(self):
+		return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+	def __eq__(self, other):
+		if other is None:
+			return False
+		return self.num == other.num and self.prime == other.prime
+
+	# 연습문제 1.1
+	def __ne__(self, other):
+		if other is None:
+			return False
+		return self.num != other.num or self.prime != other.prime
+
+	# 책 답
+	# def __ne__(self, other):
+	# 	return not(self == other)
+
+	def __add__(self, other):
+		if self.prime != other.prime:
+			raise TypeError('Cannot add two numbers in different Fields')
+		num = (self.num + other.num) % self.prime
+
+		# FieldElement 를 사용해도 되지만, 클래스 상속 시 __add__ 메서드가 실행될 때 해당 하위 클래스가 아닌
+		# FieldElement 의 인스턴스를 반환하게 고정됌
+		# return FieldElement(num, self.prime)
+
+		return self.__class__(num, self.prime)
+
+	def __sub__(self, other):
+		if self.prime != other.prime:
+			raise TypeError('Cannot sub two numbers in different Fields')
+		num = (self.num - other.num) % self.prime
+		return self.__class__(num, self.prime)
+
+	# 연습문제 1.6
+	def __mul__(self, other):
+		if self.prime != other.prime:
+			raise TypeError('Cannot mul two numbers in different Fields')
+		num = (self.num * other.num) % self.prime
+		return self.__class__(num, self.prime)
+
+	# 거듭제곱
+	def __pow__(self, exponent):
+		# num = (self.num ** exponent) % self.prime
+
+		# pow 함수 사용
+		# num = pow(self.num, exponent) % self.prime
+		num = pow(self.num, exponent, self.prime)  # 이게 더 효율적
+		return self.__class__(num, self.prime)
---
+++ chapter1/main.py
@@ -0,0 +1,70 @@
+from FieldElement import FieldElement
+
+a = FieldElement(7, 13)
+b = FieldElement(6, 13)
+print(a == b)
+print(a == a)
+
+a = FieldElement(7, 13)
+b = FieldElement(6, 13)
+print(a != b)
+
+a = FieldElement(7, 13)
+b = FieldElement(12, 13)
+c = FieldElement(6, 13)
+print(a + b == c)
+
+# 연습문제 1.2
+a = FieldElement(44, 57)
+b = FieldElement(33, 57)
+c = FieldElement(20, 57)
+print(a + b == c)
+
+a = FieldElement(9, 57)
+b = FieldElement(29, 57)
+c = FieldElement(37, 57)
+print(a - b == c)
+
+a = FieldElement(52, 57)
+b = FieldElement(30, 57)
+c = FieldElement(38, 57)
+d = FieldElement(41, 57)
+print(a - b - c == d)
+
+# 연습문제 1.4
+print((95 * 45 * 31) % 97)
+print((17 * 13 * 19 * 44) % 97)
+print((127 * ((77 ** 49) % 97)) % 97)
+print(((12 ** 7) * (77 ** 49)) % 97)
+
+# 연습문제 1.5
+prime = 19
+kSet = [1, 3, 7, 13, 18]
+tempList = []
+for k in kSet:
+	for i in range(0, prime):
+		tempList.append((k * i) % prime)
+	print(sorted(tempList))
+	tempList = []
+
+# 답
+for k in (1, 3, 7, 13, 18):
+	print(sorted([k * i % prime for i in range(prime)]))
+
+# 곱하기 구현
+a = FieldElement(3, 13)
+b = FieldElement(12, 13)
+c = FieldElement(10, 13)
+print(a * b == c)
+
+# 거듭제곱 구현
+a = FieldElement(3, 13)
+b = FieldElement(1, 13)
+print(a ** 3 == b)
+
+# 연습문제 1.7
+primes = [7, 11, 17, 31]
+
+for p in primes:
+	print([(i ** (p - 1)) % p for i in range(1, p)])
+	print([pow(i, (p - 1), p) for i in range(1, p)])  # pow 사용
